6cf66c476c0d7575216da76bbad8717c9f4c1f70
Samuel Ezedi <ezedi@Samuels-MacBook-Pro.local>
2024-12-28 13:33:57 +0100

    updates

--- app/components/goal_section.tsx
+++ app/components/goal_section.tsx
@@ -13,7 +13,7 @@ const GoalSection = () => {
           <h1 className="mt-6 text-2xl md:text-sm lg:text-xl text-sanHuanBlue font-publicSans text-center font-bold">
             Personal Finances
           </h1>
-          <p className="mt-6 text-sanHuanBlue font-publicSans text-lg text-center md:text-sm leading-8 lg:text-md">
+          <p className="mt-6 text-sanHuanBlue font-publicSans text-lg text-center md:text-sm leading-8 lg:text-xl">
             Consolidate financial data from multiple sources and categorize
             transactions up to 2 years of history. Analyze reports to reconcile
             activities in your account.
@@ -30,7 +30,7 @@ const GoalSection = () => {
           <h1 className="mt-6 text-2xl md:text-sm lg:text-xl text-sanHuanBlue font-publicSans text-center font-bold">
             Banking & Coverage
           </h1>
-          <p className="mt-6 text-sanHuanBlue font-publicSans text-lg text-center md:text-sm leading-8 lg:text-md">
+          <p className="mt-6 text-sanHuanBlue font-publicSans text-lg text-center md:text-sm leading-8 lg:text-xl">
             With our platform you can speed up account onboarding and support 
             ongoing payments for checking, savings, credit card, and brokerage account.
           </p>
@@ -45,7 +45,7 @@ const GoalSection = () => {
           <h1 className="mt-6 text-2xl md:text-sm lg:text-xl text-sanHuanBlue font-publicSans text-center font-bold">
             Consumer Payments
           </h1>
-          <p className="mt-6 text-sanHuanBlue font-publicSans text-lg md:text-sm text-center leading-8 lg:text-md">
+          <p className="mt-6 text-sanHuanBlue font-publicSans text-lg md:text-sm text-center leading-8 lg:text-xl">
           It’s easier to set up secure bank payments with us through a flow designed with 
           the user experience in mind. Customers could 
           instantly authenticate their account.
